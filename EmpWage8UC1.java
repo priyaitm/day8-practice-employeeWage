70c859c795944a65409502332df5ab79247cf507
priya <priyachauhan47365@gmail.com>
2023-02-07 14:58:45 -0600

    Added:Welcome to Employee Wage Computation Program!!

---
+++ EmpWage8UC1.java
@@ -0,0 +1,10 @@
+package day8prob.employee.wage;
+
+import day8practice.employee.EmployeeWage8;
+
+public class EmpWage8UC1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+
+}
